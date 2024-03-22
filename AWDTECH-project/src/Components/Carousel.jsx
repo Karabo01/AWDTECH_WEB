bcc95b37c875d7649a24eb4e3ba48cb51a5f025e
Karabo <morena.awdtech@gmail.com>
2024-03-23 01:09:22 +0200

    Carousel added

--- AWDTECH-project/src/Components/Carousel.jsx
+++ AWDTECH-project/src/Components/Carousel.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { CarouselItem } from "./CarouselItem";
-import internet from "./assets/internet.png"
-import cloudserver from "./assets/cloud-server.png"
-import web from "./assets/web.png"
-
+import internet from "./assets/internet.png";
+import cloudserver from "./assets/cloud-server.png";
+import web from "./assets/web.png";
+import { useState } from "react";
+import './css/Carousel.css';
 export const Carousel = () =>{
-
+    
+    const [activeIndex, setActiveIndex] = useState(0);
     const items = [
        {
         title: "Web Hosting",
@@ -24,14 +26,60 @@ export const Carousel = () =>{
         icon :web ,
         description : "This is what we do and stuff"
        }
-    ]
+    ];
+
+    const updateIndex =(newIndex) =>{
+        if(newIndex <0){
+            newIndex = 0;
+        }else if(newIndex >= items.length){
+            newIndex = 0;
+        }
+        setActiveIndex(newIndex);
+    }
+    
+    
+
     return(
-        <div className="Carousel">
-            <div className="inner">
+        <div className="carousel">
+            <div className="inner" style = {{transform: `translate(-${activeIndex * 100}%)`}}>
                 {items.map((item) =>{
                     return  <CarouselItem item={item}/> ;
                 })}
            
+            </div>
+
+            <div className="carousel-buttons">
+                <button onClick={() =>{
+                  updateIndex(activeIndex - 1)
+                }} className="button-arrow"> 
+                <span className="material-symbols-outlined">
+                   arrow_back_ios
+                </span>
+                </button>
+
+                <div className="indicators">
+                 {items.map((item, index) =>{
+
+                    return(
+                        <button onClick ={() =>{
+                            updateIndex(index)
+                        }}className="indicator-buttons">
+                    <span className={`material-symbols-outlined ${index===activeIndex? "indicator-symbol-active": "indicator-symbol"}`}>
+                        radio_button_checked
+                    </span>
+                    </button>
+                    );
+                 })}
+                    
+                   
+                </div>
+                <button onClick={() =>{
+                  updateIndex(activeIndex + 1)
+                }}className="button-arrow">
+                <span className="material-symbols-outlined">
+                      arrow_forward_ios
+                       </span>
+                </button>
             </div>
         </div>
       
